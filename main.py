bc0aac98b465b53f2ecc1a1eeb453a2d99b0d391
Veronika777 <vginzburg.a@gmail.com>
2024-04-01 20:29:46 +0300

    add score

--- main.py
+++ main.py
@@ -19,6 +19,10 @@ target_y = random.randint(0,SCREEN_HEIGHT - target_height)
 
 color = (random.randint(0,255), random.randint(0, 255), random.randint(0, 255))
 
+score = 0
+
+font = pygame.font.Font(None, 36)
+
 running = True
 
 while running:
@@ -29,9 +33,14 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                score += 1
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
     screen.blit(target_image, (target_x, target_y))
+
+    score_text = font.render("Score: " + str(score), True, (255, 255, 255))
+    screen.blit(score_text, (10, 10))
+
     pygame.display.update()
 pygame.quit()
 
